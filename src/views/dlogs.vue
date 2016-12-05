8e6c3f0949a05cd051b426388174458a215d9e38
姜德奇 <jiangdeqi@iscs.com.cn>
2016-12-05 16:54:37 +0800

    格式整理

--- src/views/dlogs.vue
+++ src/views/dlogs.vue
@@ -2,45 +2,41 @@
   <div class="block">
     <span class="demonstration">默认</span>
     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-    <div>23542345<input type="text"/></div>
-<el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
-
-<el-dialog title="收货地址" v-model="dialogTableVisible">
-  <el-table :data="gridData">
-    <el-table-column property="date" label="日期" width="150"></el-table-column>
-    <el-table-column property="name" label="姓名" width="200"></el-table-column>
-    <el-table-column property="address" label="地址"></el-table-column>
-  </el-table>
-</el-dialog>
-
-<!-- Form -->
-<el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
-
-<el-dialog title="收货地址" v-model="dialogFormVisible">
-  <el-form :model="form">
-    <el-form-item label="活动名称" :label-width="formLabelWidth">
-      <el-input v-model="form.name" auto-complete="off"></el-input>
-    </el-form-item>
-    <el-form-item label="活动区域" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-  </el-form>
-  <div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-  </div>
-</el-dialog>
-
-
+    <div>23542345<input type="text" /></div>
+    <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+    <el-dialog title="收货地址" v-model="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column property="date" label="日期" width="150"></el-table-column>
+        <el-table-column property="name" label="姓名" width="200"></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
+    <!-- Form -->
+    <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+    <el-dialog title="收货地址" v-model="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <style>
-.el-dialog {
-  margin-top:150px;
-}
+  .el-dialog {
+    margin-top: 150px;
+  }
+
 </style>
 <script>
   export default {
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,41 +1,37 @@
 <template>
-    <el-row >
-      <el-col :span="6" class="mrg10T pad10L pad10R">
-        <div class="grid-content pad10A bg-ccc height-300">
-          <el-col :span="8" v-for="list in cardEntry" class="mrg10T pad10L pad10R">
-            <p><i class="el-icon-circle-check"></i>{{list.name}}{{list.id}}</p>
-            <p>{{list.msg}}</p>
-            <el-button @click.native="add(list)">增加</el-button>
-          </el-col>
-        </div>
-      </el-col>
-
-      <el-col :span="6" v-for="list in cardList" class="mrg10T pad10L pad10R">
-        <div class="grid-content pad10A bg-ccc height-300">
-          <p><i class="el-icon-star-on"></i>{{list.name}}</p>
+  <el-row>
+    <el-col :span="6" class="mrg10T pad10L pad10R">
+      <div class="grid-content pad10A bg-ccc height-300">
+        <el-col :span="8" v-for="list in cardEntry" class="mrg10T pad10L pad10R">
+          <p><i class="el-icon-circle-check"></i>{{list.name}}{{list.id}}</p>
           <p>{{list.msg}}</p>
-        </div>
-      </el-col>
-
-      <el-col :span="6" class="mrg10T pad10L pad10R">
-        <div class="grid-content pad10A bg-ccc height-300">
-          <el-radio class="radio" v-model="radio" label="1">备选项1</el-radio>
-          <el-radio class="radio" v-model="radio" label="2">备选项2</el-radio>
-          <el-select v-model="value" style="width:240px;" >
-            <el-option v-for="item in options" :label="item.label" :value="item.value" @click.native="tiemok(item)">
-            </el-option>
-          </el-select>
-          
-        </div>
-      </el-col>
-      <el-col :span="6" class="mrg10T pad10L pad10R">
-        <div class="grid-content pad10A bg-ccc height-300">
-          <el-progress class="mrg25T" type="circle" :percentage="25"></el-progress>
-        </div>
-      </el-col>
-    </el-row>
+          <el-button @click.native="add(list)">增加</el-button>
+        </el-col>
+      </div>
+    </el-col>
+    <el-col :span="6" v-for="list in cardList" class="mrg10T pad10L pad10R">
+      <div class="grid-content pad10A bg-ccc height-300">
+        <p><i class="el-icon-star-on"></i>{{list.name}}</p>
+        <p>{{list.msg}}</p>
+      </div>
+    </el-col>
+    <el-col :span="6" class="mrg10T pad10L pad10R">
+      <div class="grid-content pad10A bg-ccc height-300">
+        <el-radio class="radio" v-model="radio" label="1">备选项1</el-radio>
+        <el-radio class="radio" v-model="radio" label="2">备选项2</el-radio>
+        <el-select v-model="value" style="width:240px;">
+          <el-option v-for="item in options" :label="item.label" :value="item.value" @click.native="tiemok(item)">
+          </el-option>
+        </el-select>
+      </div>
+    </el-col>
+    <el-col :span="6" class="mrg10T pad10L pad10R">
+      <div class="grid-content pad10A bg-ccc height-300">
+        <el-progress class="mrg25T" type="circle" :percentage="25"></el-progress>
+      </div>
+    </el-col>
+  </el-row>
 </template>
-
 <script>
   // 引入子模板
   // import Hello from './components/hello'
