91900bdb7a95074b7816e0b70615bb775a254ce0
姜德奇 <jiangdeqi@iscs.com.cn>
2016-12-22 15:15:59 +0800

    新增个列表循环

--- src/views/list.vue
+++ src/views/list.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <el-card class="box-card mrg10T mrg10L">
     <div class="text item">
       <el-date-picker v-model="value1" type="date" placeholder="选择日期" :picker-options="pickerOptions0">
@@ -6,6 +7,14 @@
       <el-button :plain="true" @click.native="open">打开消息提示</el-button>
     </div>
   </el-card>
+  <el-row>
+    <el-col :span="6" v-for="itm in menus" class="mrg10T pad10L pad10R">
+      <div class="grid-content pad10A bg-ccc min-height250">
+     {{itm.name}}
+      </div>
+    </el-col>
+  </el-row>
+  </div>
 </template>
 <style scoped>
   .text {
@@ -22,7 +31,20 @@
   export default {
     data() {
       return {
-        value1: ''
+        value1: '',
+        menus:[{
+          name:"良心",
+          id:"1"
+        },{
+          name:"铁球",
+          id:"2"
+        },{
+          name:"黑心",
+          id:"3"
+        },{
+          name:"棒球手",
+          id:"4"
+        }]
       };
     },
       methods: {
